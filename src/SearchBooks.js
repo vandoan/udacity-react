43801c7bc729897c14308cfcb6a19031f95c4023
Van Doan <van.doan@mindbodyonline.com>
2018-02-18 10:24:53 -0800

    search author fix

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -59,7 +59,7 @@ class SearchBooks extends Component {
 		                    </div>
 		                  </div>
 		                  <div className="book-title">{book.title}</div>
-		                  <div className="book-authors">{book.author}</div>
+		                  <div className="book-authors">{book.authors}</div>
 		                </div>
 		              </li>
 		            )) }
